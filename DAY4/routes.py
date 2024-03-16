fc57c8358d2e73ab76e2fcf1c8446d7ae25a47b6
Neivanny1 <nevileneivanny@gmail.com>
2024-03-16 20:34:48 +0000

    added ticket posting functionaltiy

--- DAY4/routes.py
+++ DAY4/routes.py
@@ -12,23 +12,30 @@ def home():
 '''
 Function for adding a new task
 '''
-@app.route('/tasks', methods=['POST'])
+@app.route('/tasks', methods=['POST', 'GET'])
 def add_task():
-  data = request.json
-  task_name  = data.get('task_name')
-  task_status = data.get('task_status')
-  task_feedback = data.get('task_feedback')
-  time_created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-  if task_name and task_status and task_feedback:
+  if request.method == 'POST':
+    data = request.json
+    task_name  = data.get('task_name')
+    task_status = data.get('task_status')
+    task_feedback = data.get('task_feedback')
+    time_created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if task_name and task_status and task_feedback:
+      tasks = read_tasks()
+      task_id = len(tasks) + 1
+      tasks[task_id] = {
+        "task_name": task_name,
+        "task_status": task_status,
+        "task_feedback": task_feedback,
+        "time_created": time_created
+      }
+      write_tasks(tasks)
+      return jsonify({"Message": {"Status Code": 201, "Event": f"Task with ID {task_id} created","Time" : f"{time_created}"}}), 201
+    else:
+      return jsonify({"error": "Forbidden: All fields (task_name, task_status, task_feedback) are required."}), 403
+  elif request.method == 'GET':
+    '''
+    Geting all tasks
+    '''
     tasks = read_tasks()
-    task_id = len(tasks) + 1
-    tasks[task_id] = {
-      "task_name": task_name,
-      "task_status": task_status,
-      "task_feedback": task_feedback,
-      "time_created": time_created
-    }
-    write_tasks(tasks)
-    return jsonify({"Message": {"Status Code": 201,"Event": f"Task with ID {task_id} created","Time" : f"{time_created}"}}), 201
-  else:
-     return jsonify({"error": "Forbidden: All fields (task_name, task_status, task_feedback) are required."}), 403
+    return jsonify(tasks)
